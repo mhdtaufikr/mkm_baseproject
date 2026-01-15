8993c8dc5324654b4180c6415e2ab879f4f1f411
Algi Fari Ramdhani <algifarir11112002@gmail.com>
2026-01-15 16:39:29 +0700

    chore: init proj

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DropdownController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\RulesController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +18,35 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [AuthController::class, 'login'])->name('login');
+Route::any('/auth/login', [AuthController::class, 'postLogin']);
+Route::get('auth/microsoft', [AuthController::class, 'handleAzureCallback'])->name('login.azzure');
+Route::get('/logout', [AuthController::class, 'logout']);
+Route::post('request/access', [AuthController::class, 'requestAccess']);
+
+Route::middleware(['auth'])->group(function () {
+    Route::post('/change-password', [AuthController::class, 'changePassword'])->name('changePassword');
+    //Home Controller
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+    /* Route::get('/wa', [HomeController::class, 'wa'])->name('wa'); */
+
+    //Dropdown Controller
+    Route::get('/dropdown', [DropdownController::class, 'index']);
+    Route::post('/dropdown/store', [DropdownController::class, 'store']);
+    Route::patch('/dropdown/update/{id}', [DropdownController::class, 'update']);
+    Route::delete('/dropdown/delete/{id}', [DropdownController::class, 'delete']);
+
+    //Rules Controller
+    Route::get('/rule', [RulesController::class, 'index']);
+    Route::post('/rule/store', [RulesController::class, 'store']);
+    Route::patch('/rule/update/{id}', [RulesController::class, 'update']);
+    Route::delete('/rule/delete/{id}', [RulesController::class, 'delete']);
+
+    //User Controller
+    Route::get('/user', [UserController::class, 'index'])->name('user.index');
+    Route::post('/user/store', [UserController::class, 'store']);
+    Route::post('/user/store-partner', [UserController::class, 'storePartner']);
+    Route::patch('/user/update/{user}', [UserController::class, 'update']);
+    Route::get('/user/revoke/{user}', [UserController::class, 'revoke']);
+    Route::get('/user/access/{user}', [UserController::class, 'access']);
 });
